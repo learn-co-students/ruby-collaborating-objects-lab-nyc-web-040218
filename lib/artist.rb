c2a1cd52bb079cf8eccd2a22ce9491143a7847ec
LaneMiller <Chris.Lane.Miller@gmail.com>
2018-04-07 18:15:07 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,16 @@
 class Artist
-  attr_reader :name
+  attr_accessor :name
+  attr_reader :songs
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self
-  end
-
-  def name=(name)
-    @name = name
+    self.save
   end
 
   def add_song(song)
     @songs << song
-  end
-
-  def songs
-    @songs
   end
 
   def save
@@ -29,19 +22,19 @@ class Artist
   end
 
   def self.find_artist_by_name(name)
-    all.find {|artist| artist.name.include?(name)}
+    all.find {|artist| artist.name == (name)}
   end
 
   def self.find_or_create_by_name(name)
-    if find_artist_by_name(name) == nil
-      Artist.new(name)
-    else
+    if find_artist_by_name(name)
       find_artist_by_name(name)
+    else
+      Artist.new(name)
     end
   end
 
   def print_songs
-    self.songs.each {|song| puts song.name}
+    songs.each {|song| puts song.name}
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,16 @@
 class Song
-  attr_reader :name, :artist
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
-  def name=(title)
-    @name = title
-  end
-
-  def artist=(artist_name)
-    @artist = artist_name
-  end
-
   def self.new_by_filename(file_name)
-    file_name = file_name.split(" - ")
-    song = Song.new(file_name[1])
-    song.artist = Artist.find_or_create_by_name(file_name[0])
-    song.artist.add_song(song)
-    song
+    artist, song = file_name.split(" - ")
+    new_song = self.new(song)
+    new_song.artist = Artist.find_or_create_by_name(artist)
+    new_song.artist.add_song(new_song)
+    new_song
   end
 
 end
